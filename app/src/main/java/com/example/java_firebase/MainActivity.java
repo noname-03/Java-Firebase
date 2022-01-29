b4031fb9abd18142a969f580c94cd84953c3e1da
noname-03 <fahrurrozi.arul3@gmail.com>
2022-01-29 12:16:16 +0700

    create data object via handler

--- app/src/main/java/com/example/java_firebase/MainActivity.java
+++ app/src/main/java/com/example/java_firebase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -13,16 +14,27 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference table;
+    EditText kodeMK, namaMK ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //deklarasi
+        kodeMK = findViewById(R.id.Idtxt);
+        namaMK = findViewById(R.id.Nametxt);
     }
     public void simpan(View view){
-        database = FirebaseDatabase.getInstance();
-        table = database.getReference("tb_mahasiswa");
-        table.setValue("Testing");
+        database = FirebaseDatabase.getInstance(); //mecari database
+        table = database.getReference("tb_mahasiswa"); //mencari table
+
+        String kode = kodeMK.getText().toString();
+        String nama = namaMK.getText().toString();
+
+        DataHandler handler = new DataHandler(kode, nama);
+        table.child(kode).setValue(handler);
+//        table.setValue("Fadil");
         Toast.makeText(MainActivity.this, "Berhasil menyimpan data", Toast.LENGTH_SHORT).show();
     }
 }
